9ecaf2f3bff0c9a5c51aba73852ebb88702e5dc7
Liam <mciverliam@gmail.com>
2026-03-17 17:01:20 +0000

    Amended location

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,7 +4,7 @@ locals {
   # Resource names
   resource_group_name = "${local.prefix}-rg"
   vnet_name           = "${local.prefix}-vnet"
-  kv_name             = "${local.prefix}-kv2"
+  kv_name             = "${local.prefix}-akv"
   law_name            = "${local.prefix}-law"
   app_insights_name   = "${local.prefix}-ai"
   function_name       = "${local.prefix}-func"
